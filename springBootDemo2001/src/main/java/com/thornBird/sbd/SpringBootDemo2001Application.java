0b69e8b10637d64045ca654b4c41285ca6fd0b3b
Administrator <Administrator@SKY-20181218HRK>
2020-04-21 01:21:33 +0800

    "2020-04-20:commit spring boot project"

---
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/SpringBootDemo2001Application.java
@@ -0,0 +1,13 @@
+package com.thornBird.sbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo2001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo2001Application.class, args);
+	}
+
+}
---
+++ springBootDemo2001/src/main/java/com/thornBird/sbd/modules/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.thornBird.sbd.modules.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test/appDesc")
+	@ResponseBody
+	public String getDesc(){
+		return "Hello world!Welcome to the first spring boot";
+	}
+
+}
